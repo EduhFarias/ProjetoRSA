3bb18e1c3952f552cc45ad7a7094f2636205f949
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2019-04-11 00:00:26 -0300

    Update

---
+++ Rsa.py
@@ -0,0 +1,63 @@
+import sympy
+
+
+def xmdc(a, b):
+    if b == 0:
+        return [1, 0, a]
+    else:
+        x, y, d = xmdc(b, a % b)
+        return [y, x - (a // b) * y, d]
+
+class Rsa:
+
+    def __init__(self):
+        self.publicKey = []
+        self.privateKey = []
+        self.message = ""
+
+    def setup(self):
+        p = sympy.randprime(10, 100)
+        q = sympy.randprime(10, 100)
+        n = p * q
+        totient = (p - 1) * (q - 1)
+        e = self.mdc(totient)
+
+        d, p, o = xmdc(e, totient)
+        print("-1 ", d)
+        if d < 0:
+            d = d % n
+            d += n
+            print("-2 ", d)
+        self.publicKey = [n, e]
+        self.privateKey = [n, d]
+
+    def encrypt(self, text):
+        message = ""
+        for i in range(len(text)):
+            message += chr((ord(text[i]) ** self.publicKey[1]) % self.publicKey[0])
+        self.message = message
+        print(self.message)
+
+    def decrypt(self):
+        message = ""
+        for i in range(len(self.message)):
+            message += chr((ord(self.message[i]) ** self.privateKey[1]) % self.privateKey[0])
+            print(ord(self.message[i]), ord(message[i]))
+        print(message)
+
+    def mdc(self, totient):
+        atual = 0
+        e = -1
+        while atual != 1:
+            ant = totient
+            atual = sympy.randprime(2, 40)
+            resto = ant % atual
+            e = atual
+
+            while resto != 0:
+                ant = atual
+                atual = resto
+                resto = ant % atual
+
+        return e
+
--- teste.py
+++ teste.py
@@ -1,9 +1,28 @@
 import sympy
+from Rsa import *
 
 #Cifração -------------------------------------------------------------
 def main():
-    p = sympy.randprime(10,100)
-    q = sympy.randprime(10,100)
+    """print("0. Criar chaves\n1. Cifrar mensagem\n2. Decifrar mensagem\n3. Encerrar")
+    choice = int(input())
+    newKey = Rsa()
+    while choice != 3:
+        if choice == 0:
+            newKey.setup()
+            print(newKey.publicKey, newKey.privateKey)
+        elif choice == 1:
+            print("Mensagem a ser cifrada:")
+            newKey.encrypt(input())
+        elif choice == 2:
+            newKey.decrypt()
+        else:
+            print("Encerrando..")
+        print("0. Criar chaves\n1. Cifrar mensagem\n2. Decifrar mensagem\n3. Encerrar")
+        choice = int(input())
+
+    """
+    p = sympy.randprime(10, 50)
+    q = sympy.randprime(10, 50)
     n = p * q
     totiente = (p-1) * (q-1)
 
@@ -11,13 +30,13 @@ def main():
 
     atual = 0
     e = -1
-    while(atual != 1):
+    while atual != 1:
         ant = totiente
-        atual = sympy.randprime(2,80)
+        atual = sympy.randprime(2, 40)
         resto = ant % atual
         e = atual
 
-        while(resto != 0):
+        while resto != 0:
             ant = atual
             atual = resto
             resto = ant % atual
@@ -25,20 +44,24 @@ def main():
     print("MDC(%d,%d) = %d" %(totiente, e, atual))
     publicKey = [totiente, e]
 
-    text = "teste"
+    text = input()
     saida = []
-    for i in range(5):
+
+    for i in range(len(text)):
         a = ord(text[i])
         conv = (a ** e) % n
         saida.append(conv)
-        print("O: %d, T: %d" %(a, conv))
+        print("O: %d %c, T: %d %c" %(a, a, conv, conv))
 
-    o,p,d = xmdc(13,640)
-    print(o,p,d)
+    o, p, d = xmdc(e, totiente)
+    print(o, p, d)
 
-    for i in range(5):
+    for i in range(len(text)):
+        if o < 0:
+            o = o % totiente
+            o += totiente
         conv = (saida[i] ** o) % n
-        print(chr(conv))
+        print(chr(conv), conv)
 
 #Cifração -------------------------------------------------------------
 
@@ -46,13 +69,13 @@ def main():
 
 # chave privada(d,n) d é o coeficiente para decifrar e n o conjunto, pode ser n ou p e q
 
-def xmdc(a,b):
-    if b==0:
-        return [1,0,a]
-    else:
-        x,y,d=xmdc(b, a%b)
-        return [y,x-(a//b)*y,d]
 
+def xmdc(a, b):
+    if b == 0:
+        return [1, 0, a]
+    else:
+        x, y, d = xmdc(b, a % b)
+        return [y, x - (a // b) * y, d]
 #Decifração -------------------------------------------------------------
 
 if __name__ == '__main__':
