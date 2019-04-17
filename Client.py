a8eedfef2eff01de01f7dd2251c0c876ea1d6de8
Eduardo Henrique Farias Silva <ehfs@ic.ufal.br>
2019-04-17 10:42:26 -0300

    Create Client.py

---
+++ Client.py
@@ -0,0 +1,20 @@
+
+
+class Client:
+	
+  name = ""
+  password = ""
+  # users = []
+	
+  def __init__(self, name, password):
+    self.name = name
+    self.password = password
+  	
+	def addUser(self, newUser):
+		self.users.append(newUser)
+		
+	def removeUser(self):
+		print("teste")
+		
+	def checkUser(self):
+		print("teste")
